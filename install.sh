bba12d71b5f4511e7cfe9f22d0a2519b2f17f253
R. Guillermo Koch <festadmin@MacBook-Pro-de-R.local>
2020-06-08 18:11:40 -0300

    correct script error with if clauses

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
 
-if [ cat /etc/crontab | grep "vpn-report.sh" ]; then
+if cat /etc/crontab | grep "vpn-report.sh"; then
     # Script is installed, uninstall?
     echo -n "Are you sure you want to uninstall? (yes/NO) "
     read decision
-    if [ "$decision" == "yes" ]; then
+    if "$decision" == "yes"; then
         echo -n "Uninstalling..."
         sed -i "vpn-report.sh" /etc/crontab
     fi
@@ -17,8 +17,7 @@ else
     # Script is not installed, install?
     echo -n "Are you sure you want to install? (yes/NO) "
     read decision
-    echo -n "Your decision: $decision"
-    if [ "$decision" == "yes" ]; then
+    if "$decision" == "yes" ; then
         cp ./vpn-report-sample.sh ./vpn-report.sh
         echo -n "Adding to crontab..."
         echo -e "0 21\t* * *\troot\tsh $SCRIPTPATH/vpn-report.sh" >> /etc/crontab
