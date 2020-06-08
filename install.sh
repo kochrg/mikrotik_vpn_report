ad296de545b357313326a61aa995c4d98a59901f
R. Guillermo Koch <festadmin@MacBook-Pro-de-R.local>
2020-06-08 17:25:32 -0300

    Uploading project

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
+if cat /etc/crontab | grep "vpn-report.sh"; then
+    # Script is installed, uninstall?
+    echo -n "Are you sure you want to uninstall? (yes/NO) "
+    read decision
+    if $decision == "yes"; then
+        echo -n "Uninstalling..."
+        sed -i "vpn-report.sh" /etc/crontab
+    fi
+else
+    # Script is not installed, install?
+    echo -n "Are you sure you want to install? (yes/NO) "
+    read decision
+
+    if $decision == 'yes'; then
+        cp ./vpn-report-sample.sh ./vpn-report.sh
+        echo -n "Adding to crontab..."
+        echo -e "0 *\t* * *\troot\tsh $SCRIPTPATH/vpn-report.sh" >> /etc/crontab
+        echo -n "Remember to edit the file $SCRIPTPATH/vpn-report.sh for personnal config."
+    fi
+fi
+
+echo -n "DONE...."
---
+++ vpn-report-sample.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Edit PREFIX, EMAIL, REPORT_DAY variables with your own
+# PREFIX: the prefix you use in your MikroTik config log
+PREFIX=
+# Email you want to send the report
+FROM_NAME=          # your name
+EMAIL_FROM=         # your email
+EMAIL_TO=           # dest email
+# Day of month you want to send the report
+REPORT_DAY=
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+
+if $day > $REPORT_DAY; then
+    if $month == '12'; then
+        year=(date -d "$(date +%Y-%m-1) 1 year" +%-Y)
+    fi
+    month=(date -d "$(date +%Y-%m-1) 1 month" +%-m)
+fi
+
+if ! test -d "./reports"; then
+    mkdir ./reports
+fi
+
+if ! test -d "./reports/$year"; then
+    mkdir ./reports/$year
+fi
+
+if ! test -d "./reports/$year/$month"; then
+    mkdir ./reports/$year/$month
+fi
+
+echo cat /var/log/syslog | grep $PREFIX | grep connected >> ./reports/$year/$month/report.txt
+
+if $day == $REPORT_DAY; then
+    # Send report in email
+    mail -s "VPN Access report" -a FROM:$FROM_NAME\<$EMAIL_FROM\> $EMAIL_TO < ./reports/$year/$month/report.txt
+fi
